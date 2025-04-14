64f42bebf2eb7128cc6f8066ece16f8069989232
gydnjs <khw33352182@gmail.com>
2025-04-14 12:01:05 +0900

    refactor : change variables name
    상수 이름 바꿈

--- GameServer/SendSocket.cs
+++ GameServer/SendSocket.cs
@@ -11,16 +11,16 @@ namespace GameServer
     internal class SendSocket
     {
         //패킷의 크기가 1024바이트 이상이면 세그먼트로 분리해서 보내야 하기 때문에 필요한 변수임
-        const int MAXPACKETLENGTH = 1024;
+        const int MaxPacketLength = 1024;
         public SendSocket()
         {
             
         }
         //패킷을 세그먼트로 분리해 만들어주는 함수
-        public void Send(byte[] data, Socket target)
+        public void SendPacket(byte[] data, Socket target)
         {
 
-            int segmentCount = data.Length / MAXPACKETLENGTH;
+            int segmentCount = data.Length / MaxPacketLength;
             int dataSize = data.Length;
             if (segmentCount == 0)
             {
@@ -33,12 +33,12 @@ namespace GameServer
             }
             for (int i = 0; i < segmentCount; i++)
             {
-                dataSize -= MAXPACKETLENGTH;
+                dataSize -= MaxPacketLength;
                 ArraySegment<byte> segment;
-                if (dataSize < MAXPACKETLENGTH)
-                    segment = new ArraySegment<byte>(data, MAXPACKETLENGTH * i, dataSize);
+                if (dataSize < MaxPacketLength)
+                    segment = new ArraySegment<byte>(data, MaxPacketLength * i, dataSize);
                 else
-                    segment = new ArraySegment<byte>(data, MAXPACKETLENGTH * i, dataSize);
+                    segment = new ArraySegment<byte>(data, MaxPacketLength * i, dataSize);
                 GameServer.SendPacketJobProcessor.Push(async () =>
                 {
                     await SendAsync(segment, target);
