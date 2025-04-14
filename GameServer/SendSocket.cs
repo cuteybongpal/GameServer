1f844e7b7517afb76f7213a55f717acb1c258ff8
gydnjs <khw33352182@gmail.com>
2025-04-14 11:45:29 +0900

    feat : add Send Packet function
    패킷을 세그먼트로 만들어 비동기로 보내는 함수를 만들었다.

--- GameServer/SendSocket.cs
+++ GameServer/SendSocket.cs
@@ -10,27 +10,45 @@ namespace GameServer
 {
     internal class SendSocket
     {
-        Socket sendSocket;
         //패킷의 크기가 1024바이트 이상이면 세그먼트로 분리해서 보내야 하기 때문에 필요한 변수임
         const int MAXPACKETLENGTH = 1024;
-        public SendSocket(IPEndPoint endPoint)
+        public SendSocket()
         {
-            sendSocket = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
-            sendSocket.Bind(endPoint);
+            
         }
-
-        public void Send(byte[] data)
+        //패킷을 세그먼트로 분리해 만들어주는 함수
+        public void Send(byte[] data, Socket target)
         {
+
             int segmentCount = data.Length / MAXPACKETLENGTH;
-            if (segmentCount <= 0)
+            int dataSize = data.Length;
+            if (segmentCount == 0)
             {
                 ArraySegment<byte> segment = new ArraySegment<byte>(data, 0, data.Length);
-                sendSocket.SendAsync(segment);
+                GameServer.SendPacketJobProcessor.Push(async () =>
+                {
+                    await SendAsync(segment, target);
+                });
+                return;
             }
             for (int i = 0; i < segmentCount; i++)
             {
-                ArraySegment<byte> segment = new ArraySegment<byte>();
+                dataSize -= MAXPACKETLENGTH;
+                ArraySegment<byte> segment;
+                if (dataSize < MAXPACKETLENGTH)
+                    segment = new ArraySegment<byte>(data, MAXPACKETLENGTH * i, dataSize);
+                else
+                    segment = new ArraySegment<byte>(data, MAXPACKETLENGTH * i, dataSize);
+                GameServer.SendPacketJobProcessor.Push(async () =>
+                {
+                    await SendAsync(segment, target);
+                });
             }
+        }
+
+        async Task SendAsync(ArraySegment<byte> packet, Socket target)
+        {
+            await target.SendAsync(packet);
         }
     }
 }
