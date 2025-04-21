0ea629fe80acfa0a89240327eb0e51f3e1072647
hyowonKim <khw33352182@gmail.com>
2025-04-21 12:24:59 +0900

    feat : add panel

---
+++ GameServer/GameRoom/Panel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.GameRoom
+{
+    internal class Panel : Entity
+    {
+        public Panel(Action updateAction)
+        {
+            updateAction += Update;
+        }
+        void Update()
+        {
+            //if ()
+        }
+    }
+}
